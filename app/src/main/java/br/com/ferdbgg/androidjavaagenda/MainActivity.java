94df03e06844c97a24b29046621958743ee4b322
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-05-04 21:42:44 -0300

    Initial commit

---
+++ app/src/main/java/br/com/ferdbgg/androidjavaagenda/MainActivity.java
@@ -0,0 +1,19 @@
+package br.com.ferdbgg.androidjavaagenda;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        Toast.makeText(this, "Esse é um alerta", Toast.LENGTH_LONG).show();
+
+    }
+}
