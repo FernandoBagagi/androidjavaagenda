adb048cdb1f62aea25ba6ca45095a3f58f64ccd2
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-05-13 15:34:19 -0300

    cria model para Aluno

---
+++ app/src/main/java/br/com/ferdbgg/androidjavaagenda/models/pojos/Aluno.java
@@ -0,0 +1,17 @@
+package br.com.ferdbgg.androidjavaagenda.models.pojos;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+import br.com.ferdbgg.androidjavaagenda.models.enums.GeneroEnum;
+
+public record Aluno(
+        Integer id,
+        String nome,
+        String email,
+        String telefone,
+        GeneroEnum genero
+) implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+}
