5e519e5dbaff9ed4ff974cc7f6eab5398cb09277
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-05-13 09:53:31 -0300

    cria formulario aluno

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.legacy.support.v4)
     implementation(libs.cardview)
     implementation(libs.constraintlayout)
+    implementation(libs.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
