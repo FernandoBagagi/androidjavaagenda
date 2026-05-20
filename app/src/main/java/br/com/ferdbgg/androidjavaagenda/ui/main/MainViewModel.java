ae184711ee285c068c27955feeaea309ab886344
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-05-19 22:12:22 -0300

    adiciona o comportamento à MainViewModel, deixando praparado para quando for carregar os dados do banco

--- app/src/main/java/br/com/ferdbgg/androidjavaagenda/ui/main/MainViewModel.java
+++ app/src/main/java/br/com/ferdbgg/androidjavaagenda/ui/main/MainViewModel.java
@@ -1,4 +1,31 @@
 package br.com.ferdbgg.androidjavaagenda.ui.main;
 
-public class MainViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.Arrays;
+import java.util.List;
+
+import br.com.ferdbgg.androidjavaagenda.models.enums.GeneroEnum;
+import br.com.ferdbgg.androidjavaagenda.models.pojos.Aluno;
+
+public class MainViewModel extends ViewModel {
+
+    private final MutableLiveData<List<Aluno>> alunosLiveData = new MutableLiveData<>();
+
+    public LiveData<List<Aluno>> getAlunosLiveData() {
+        return alunosLiveData;
+    }
+
+    public void carregarAlunos() {
+        //TODO: pegar do banco de dados ou api
+        List<Aluno> mockAlunos = Arrays.asList(
+                new Aluno(1, "Maria", "maria@email.com", "11999999999", GeneroEnum.FEMININO),
+                new Aluno(2, "João", "joao@email.com", "11888888888", GeneroEnum.MASCULINO),
+                new Aluno(3, "Alex", "alex@email.com", "11777777777", GeneroEnum.NAO_BINARE)
+        );
+        alunosLiveData.setValue(mockAlunos);
+    }
+
 }
