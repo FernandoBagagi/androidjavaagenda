592b3b2357df80902f6a0edc30f90b18b57ef0fc
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-05-19 22:15:04 -0300

    refatora MainActivity retirando carregamento de dados e adicionou os métodos de mudar pra próxima tela passando o aluno como parâmetro

--- app/src/main/java/br/com/ferdbgg/androidjavaagenda/ui/main/MainActivity.java
+++ app/src/main/java/br/com/ferdbgg/androidjavaagenda/ui/main/MainActivity.java
@@ -1,8 +1,8 @@
 package br.com.ferdbgg.androidjavaagenda.ui.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
@@ -10,18 +10,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 
-import java.util.Arrays;
 import java.util.List;
 
 import br.com.ferdbgg.androidjavaagenda.R;
+import br.com.ferdbgg.androidjavaagenda.models.pojos.Aluno;
+import br.com.ferdbgg.androidjavaagenda.ui.formulario.FormularioAlunoActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerAlunosView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -40,38 +44,58 @@ public class MainActivity extends AppCompatActivity {
         final MaterialToolbar toolbar = findViewById(R.id.main_top_app_bar);
         setSupportActionBar(toolbar);
 
-        final RecyclerView recyclerAlunosView = findViewById(R.id.main_lista_alunos);
+        recyclerAlunosView = findViewById(R.id.main_lista_alunos);
         // Define como lista vertical
         recyclerAlunosView.setLayoutManager(new LinearLayoutManager(this));
-        // Conecta o adapter à recycleView
-        final var listaAlunosAdapter = new ListaAlunosAdapter(this.getNomesAlunos());
-        recyclerAlunosView.setAdapter(listaAlunosAdapter);
+
+        final MainViewModel viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+
+        viewModel
+                .getAlunosLiveData()
+                .observe(this, this::alunosLiveDataObserver);
+
+        viewModel.carregarAlunos();
 
         final ExtendedFloatingActionButton fabAddAluno = findViewById(R.id.main_floating_action_button);
-        fabAddAluno.setOnClickListener(this::onClickFabAddAluno);
+        fabAddAluno.setOnClickListener(this::abrirFormularioEmModoCriacao);
 
-        Toast.makeText(this, "Esse é um alerta", Toast.LENGTH_LONG).show();
-
-    }
-
-    private List<String> getNomesAlunos() {
-        return Arrays.asList("Maria", "João", "Antônio", "José", "Ana Clara", "Emília",
-                "Ana Maria", "Felipe", "Alan", "Carla", "Luan", "Bernardo", "Ricardo",
-                "Daniela", "Omar", "Vitória", "Samara", "Roberto", "Damião", "Júlio",
-                "Tadeu", "Tamires", "Talita", "Gabriel", "Fabrício", "Jamile", "Heitor"
-        );
     }
 
     private WindowInsetsCompat addSystemBarPadding(View view, WindowInsetsCompat insets) {
+
         Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
 
         // Adiciona padding na parte de baixo igual à altura da barra de navegação do celular
         view.setPadding(0, 0, 0, systemBars.bottom);
 
         return insets;
+
     }
 
-    private void onClickFabAddAluno(View view) {
-        Toast.makeText(MainActivity.this, "Clicou em adicionar!", Toast.LENGTH_SHORT).show();
+    private void abrirFormularioEmModoEdicao(Aluno alunoClicado) {
+
+        final var intent = new Intent(this, FormularioAlunoActivity.class);
+        intent.putExtra("aluno", alunoClicado);
+        startActivity(intent);
+
     }
+
+    private void abrirFormularioEmModoCriacao(View view) {
+
+        final var intent = new Intent(this, FormularioAlunoActivity.class);
+        startActivity(intent);
+
+    }
+
+    private void alunosLiveDataObserver(List<Aluno> alunos) {
+
+        final var listaAlunosAdapter = new ListaAlunosAdapter(
+                alunos,
+                this::abrirFormularioEmModoEdicao
+        );
+
+        recyclerAlunosView.setAdapter(listaAlunosAdapter);
+
+    }
+
 }
