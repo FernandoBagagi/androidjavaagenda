6db9d542c228e09aa149643f5cf0b662decaa6b3
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-05-19 22:11:32 -0300

    refatora ListaAlunosAdapter para receber Aluno como dado da lista e acrescenta listener de quando clicar em um item

--- app/src/main/java/br/com/ferdbgg/androidjavaagenda/ui/main/ListaAlunosAdapter.java
+++ app/src/main/java/br/com/ferdbgg/androidjavaagenda/ui/main/ListaAlunosAdapter.java
@@ -10,12 +10,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
+import br.com.ferdbgg.androidjavaagenda.models.pojos.Aluno;
+
 public class ListaAlunosAdapter extends RecyclerView.Adapter<ListaAlunosAdapter.ListaAlunoViewHolder> {
 
-    private final List<String> listaDeAlunos;
+    private final List<Aluno> alunos;
+    private final OnItemListaAlunoClickListener listener;
 
-    public ListaAlunosAdapter(List<String> listaDeAlunos) {
-        this.listaDeAlunos = listaDeAlunos;
+    public ListaAlunosAdapter(
+            List<Aluno> alunos,
+            OnItemListaAlunoClickListener listener
+    ) {
+        this.alunos = alunos;
+        this.listener = listener;
     }
 
     @NonNull
@@ -35,13 +42,27 @@ public class ListaAlunosAdapter extends RecyclerView.Adapter<ListaAlunosAdapter.
 
     @Override
     public void onBindViewHolder(@NonNull ListaAlunoViewHolder holder, int position) {
-        final var nomeDoAluno = listaDeAlunos.get(position);
-        holder.nomeAluno.setText(nomeDoAluno);
+
+        final var aluno = alunos.get(position);
+
+        holder.nomeAluno.setText(aluno.nome());
+
+        holder.itemView.setOnClickListener(v -> {
+            if (listener != null) {
+                // Avisa a Activity e manda o aluno clicado
+                listener.onItemClick(aluno);
+            }
+        });
+
     }
 
     @Override
     public int getItemCount() {
-        return listaDeAlunos.size();
+        return alunos.size();
+    }
+
+    public interface OnItemListaAlunoClickListener {
+        void onItemClick(Aluno aluno);
     }
 
     public static class ListaAlunoViewHolder extends RecyclerView.ViewHolder {
@@ -52,5 +73,7 @@ public class ListaAlunosAdapter extends RecyclerView.Adapter<ListaAlunosAdapter.
             // text1 é o ID padrão do TextView dentro do simple_list_item_1
             nomeAluno = itemView.findViewById(android.R.id.text1);
         }
+
     }
+
 }
